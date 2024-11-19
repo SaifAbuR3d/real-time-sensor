a791fcde9691dff6c299c712c65f83da4517ee8d
SaifAbuR3d <saefraed2@gmail.com>
2024-11-20 00:29:38 +0200

    Fix

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-import { firebaseConfig } from "./config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBld121eyGUvN09x81kAYq8NXM2h1qd0BI",
+  authDomain: "arduino-uno-131eb.firebaseapp.com",
+  databaseURL: "https://arduino-uno-131eb-default-rtdb.firebaseio.com",
+  projectId: "arduino-uno-131eb",
+  storageBucket: "arduino-uno-131eb.firebasestorage.app",
+  messagingSenderId: "682910470146",
+  appId: "1:682910470146:web:7ac8021662f5aa90b8971b",
+  measurementId: "G-G3CXQ760W9",
+};
+
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 const HumidityRef = ref(database, "Humidity");
 const TemperatureRef = ref(database, "Temperature");
+
 
 onValue(HumidityRef, (snapshot) => {
   const humidity = snapshot.val();
